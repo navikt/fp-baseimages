30d9992341fb1079e23abed2f4480f9fe7e1883f
Michal J. Sladek <michal.sladek@nav.no>
2025-01-23 23:14:46 +0100

    docs: formatering kode

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -12,4 +12,4 @@ else
 fi
 
 set -x
-exec java ${DEFAULT_JVM_OPTS} ${JAVA_OPTS} -server  -classpath ${CLASSPATH} -jar app.jar ${RUNTIME_OPTS} $@
+exec java ${DEFAULT_JVM_OPTS} ${JAVA_OPTS} -server -classpath ${CLASSPATH} -jar app.jar ${RUNTIME_OPTS} $@
