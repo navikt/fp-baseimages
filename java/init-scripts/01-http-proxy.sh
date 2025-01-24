571bbec9416df3a060329cf13589e6e853097958
Michal J. Sladek <michal.sladek@nav.no>
2025-01-24 08:58:40 +0100

    refactor: rydder CLASSPATH variable som ikke brukes.

--- java/init-scripts/01-http-proxy.sh
+++ java/init-scripts/01-http-proxy.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 
 # inject proxy settings set by the nais platform webproxy: true
-export JAVA_OPTS="${JAVA_OPTS} ${JAVA_PROXY_OPTIONS}"
+if [ ${JAVA_PROXY_OPTIONS} ];
+then
+  export JAVA_OPTS="${JAVA_OPTS} ${JAVA_PROXY_OPTIONS}"
+  echo "01. - JAVA_PROXY_OPTIONS injected."
+else
+  echo "01. - No JAVA_PROXY_OPTIONS found."
+fi
