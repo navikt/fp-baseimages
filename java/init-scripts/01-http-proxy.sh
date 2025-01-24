45d283ec8a46b12da3920a21e7544331d37e4e98
Michal J. Sladek <michal.sladek@nav.no>
2025-01-24 12:38:21 +0100

    fix: fjerner unødvendig logging.

--- java/init-scripts/01-http-proxy.sh
+++ java/init-scripts/01-http-proxy.sh
@@ -5,6 +5,4 @@ if [ ${JAVA_PROXY_OPTIONS} ];
 then
   export JAVA_OPTS="${JAVA_OPTS} ${JAVA_PROXY_OPTIONS}"
   echo "01. - JAVA_PROXY_OPTIONS injected."
-else
-  echo "01. - No JAVA_PROXY_OPTIONS found."
 fi
